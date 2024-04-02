7b1558d56e7425c2954545b642f41f7802aa35aa
oh-harin <harin@maruw.co.kr>
2024-04-02 16:19:07 +0900

    - vue project

---
+++ src/views/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="root">
+    <div v-if="liked">좋아요 눌렀음</div>
+    <button v-else v-on:click="onClickButton">Like</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data() {
+    return {
+      liked: false
+    };
+  },
+  methods: {
+    onClickButton() {
+      this.liked = true;
+    }
+  }
+};
+</script>
